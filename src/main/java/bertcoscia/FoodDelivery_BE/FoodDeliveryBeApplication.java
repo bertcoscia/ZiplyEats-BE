52bd2463547e687fcd6d1db5e2db33aaf54e63c7
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-10-02 10:22:36 +0200

    Initial commit

---
+++ src/main/java/bertcoscia/FoodDelivery_BE/FoodDeliveryBeApplication.java
@@ -0,0 +1,13 @@
+package bertcoscia.FoodDelivery_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bertcoscia/FoodDelivery_BE/FoodDeliveryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package bertcoscia.FoodDelivery_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
