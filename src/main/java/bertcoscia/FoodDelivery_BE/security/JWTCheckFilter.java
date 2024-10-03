44f8dad4bb65eaa2289f39920a82a07b336d6088
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-10-03 11:46:43 +0200

    completes package security

---
+++ src/main/java/bertcoscia/FoodDelivery_BE/security/JWTCheckFilter.java
@@ -0,0 +1,46 @@
+package bertcoscia.FoodDelivery_BE.security;
+
+import bertcoscia.FoodDelivery_BE.entities.User;
+import bertcoscia.FoodDelivery_BE.exceptions.UnauthorizedException;
+import bertcoscia.FoodDelivery_BE.services.UsersService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JWTCheckFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTTools jwtTools;
+    @Autowired
+    private UsersService usersService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new UnauthorizedException("Please insert token in Authorization header correctly");
+        String accessToken = authHeader.substring(7);
+        jwtTools.verifyToken(accessToken);
+        String id = jwtTools.extractIdFromToken(accessToken);
+        User currentUser = this.usersService.findById(UUID.fromString(id));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+    }
+}
